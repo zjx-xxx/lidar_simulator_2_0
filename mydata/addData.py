4f02f23cf9c709fc065feea575dd75b082462ec3
Lixy <listyx.w@gmail.com>
2025-03-30 16:08:39 +0800

    补充了训练代码

---
+++ mydata/addData.py
@@ -0,0 +1,93 @@
+import os
+import re
+import pandas as pd
+
+# 获取当前目录下的 mydata 子目录的路径
+mydata_dir = os.path.join(os.getcwd(), 'mydata')
+# 获取 mydata 子目录中的所有文件
+if os.path.exists(mydata_dir):
+    file_names = os.listdir(mydata_dir)  # 获取 mydata 子目录中的所有文件名
+    print(file_names)  # 打印文件名列表
+else:
+    print(f"目录 {mydata_dir} 不存在！")
+
+# 使用正则表达式匹配所有符合 pattern 的文件名
+pattern = r'^data\d+\.csv$'  # 匹配 data 后跟一个或多个数字，再加 .csv 扩展名
+matching_files = [file for file in file_names if re.match(pattern, file)]
+
+
+# 存储所有读取的数据
+all_data = []
+file_path = "./Data.csv"
+
+if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+    df = pd.read_csv(file_path, header=None)
+else:
+    df = pd.DataFrame()  # 创建空DataFrame
+
+all_data.append(df)
+# 读取并合并所有的 CSV 文件
+for file_name in matching_files:
+    if os.path.exists(f'./labeled/{file_name}'):
+        df = pd.read_csv(f'./labeled/{file_name}', header=None)  # 读取每个 CSV 文件
+        row = df.values.flatten()  # 将列转为一维数组（行）
+        all_data.append(pd.DataFrame([row]))
+        print(f'{file_name}已添加')
+    else:
+        print(f"文件 {file_name} 不存在！")
+
+# 合并数据（按行合并）
+merged_data = pd.concat(all_data, axis=0, ignore_index=True)
+
+merged_data.to_csv('./Data.csv', header=False, index=False)
+Test = merged_data.sample(frac=0.1, random_state=42)
+Train = merged_data.drop(Test.index)
+
+# 提取最后一列数据作为 Y_train
+Y_train = Train.iloc[:, -1]
+# 提取前面所有的列作为 X_train
+X_train = Train.iloc[:, :-1]
+Y_test = Test.iloc[:, -1]
+X_test = Test.iloc[:, :-1]
+
+
+# 保存 X_train、Y_train、X_test 和 Y_test 到新的 CSV 文件
+X_train.to_csv('./X_train.csv', header=False, index=False)
+Y_train.to_csv('./Y_train.csv', header=False, index=False)
+X_test.to_csv('./X_test.csv', header=False, index=False)
+Y_test.to_csv('./Y_test.csv', header=False, index=False)
+print("X_train、Y_train、X_test 和 Y_test 已保存。")
+
+import datetime
+
+# 获取当前时间
+now = datetime.datetime.now()
+
+# 提取月、日、时、分
+month = now.strftime("%m")  # 月
+day = now.strftime("%d")  # 日
+hour = now.strftime("%H")  # 时
+minute = now.strftime("%M")  # 分
+
+# 构建文件名后缀
+file_suffix = f"{month}_{day}_{hour}_{minute}"
+
+# 假设你要保存的文件名
+filename = f"rawData{file_suffix}"
+# 文件夹路径
+folder_path = f'./{filename}'
+
+# 创建文件夹（如果文件夹已存在，os.makedirs() 不会报错）
+os.makedirs(folder_path, exist_ok=True)  # exist_ok=True 表示如果文件夹已存在不报错
+print(f"文件夹 {folder_path} 已创建")
+
+import shutil
+for file_name in matching_files:
+    # 源文件路径
+    source_file = f'./labeled/{file_name}'
+    if os.path.exists(source_file):
+        # 移动文件
+        shutil.move(source_file, folder_path)
+        print(f"文件 {source_file} 已移动到 {folder_path}")
+    else:
+        print(f"文件 {file_name} 不存在！")
---
+++ train.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+from evaluate import *
+from model import NeuralNetwork
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
+
+# 检查CUDA是否可用
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def train(model, X_train, y_train, num_epochs=10, batch_size=32, learning_rate=0.001):
+    # 转换为torch张量并将其转移到GPU上
+    print(f'Training on {device}')
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32).to(device)
+    y_train_tensor = torch.tensor(y_train, dtype=torch.long).to(device)
+
+    # 确保 y_train_tensor 是 1D 张量
+    y_train_tensor = y_train_tensor.squeeze()  # 将 y_train_tensor 转为 1D 张量
+
+    # 创建训练数据集并使用 DataLoader 进行批处理
+    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    # 定义损失函数和优化器
+    criterion = nn.CrossEntropyLoss()  # 交叉熵损失
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)  # Adam优化器
+
+    # 训练模型
+    for epoch in tqdm(range(num_epochs), desc="Training Progress"):
+        model.train()
+
+        # 遍历每个小批次
+        for batch_idx, (data, target) in enumerate(train_loader):
+            # 梯度清零
+            optimizer.zero_grad()
+
+            # 前向传播
+            outputs = model(data)
+
+            # 计算损失
+            loss = criterion(outputs, target)
+
+            # 反向传播
+            loss.backward()
+
+            # 更新参数
+            optimizer.step()
+
+        # print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+if __name__ == '__main__':
+    # 假设你有训练数据 X_train 和 y_train
+    X_train = pd.read_csv('./mydata/X_train.csv', header=None)  # 加载特征数据
+    # X_train.drop(X_train.columns[169:189], axis=1, inplace=True)#去除了车屁股部分的数据
+    X_train = X_train.values
+    y_train = pd.read_csv('./mydata/Y_train.csv', header=None).values  # 加载标签数据
+
+    # 初始化模型并将其转移到GPU
+    model = NeuralNetwork().to(device)
+
+    num_epochs = 100
+    batch_size = 64
+    # 训练模型
+    train(model, X_train, y_train, num_epochs, batch_size)
+
+    # 读取X_test 和 Y_test
+    X_test = pd.read_csv('./mydata/X_test.csv', header=None)  # 从CSV文件读取X_test
+    # X_test.drop(X_test.columns[169:189], axis=1, inplace=True)#去除了车屁股部分的数据
+    X_test = X_test.values
+    y_test = pd.read_csv('./mydata/Y_test.csv', header=None).values.flatten()  # 从CSV文件读取Y_test
+
+    # 评估模型
+    acc = evaluate(model, X_test, y_test, device)
+
+    while acc < 0.99:
+        model = NeuralNetwork().to(device)  # 重新初始化模型并转移到GPU
+        train(model, X_train, y_train, num_epochs, batch_size)
+        acc = evaluate(model, X_test, y_test, device)
+
+    # 保存训练好的模型
+    torch.save(model.state_dict(), f'./model/model_{acc * 100:.2f}acc')
+    torch.save(model.state_dict(), './model/model')
